3be15acf9903c7b474fcc484b286b5747cf6c041
Anthony Marchal <anthonymarchal2308@gmail.com>
2025-03-13 12:59:09 +0100

    Animation working on

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 3f;
+    public Animator _animator;
+    public float speed;
     public InputActionAsset _asset;
     public Rigidbody2D _rig;
     public float _axisx;
@@ -14,11 +15,15 @@ public class PlayerMovement : MonoBehaviour
     public bool _grounded = false;
     public SpriteRenderer _sprite;
     public float timer = 0f;
+    public float jumpForce;
+    private bool _hasDashed = false;
+    private bool _falling = false;
     // Start is called before the first frame update
     void Start()
     {
         _rig = this.GetComponent<Rigidbody2D>();
         _sprite = this.GetComponent<SpriteRenderer>();
+        _animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,7 +48,11 @@ public class PlayerMovement : MonoBehaviour
         }
         if (timer > 0 && _grounded == true)
         {
-            _rig.AddForce(new Vector2(0f, 13f), ForceMode2D.Impulse);
+            if (_falling == false)
+            {
+                _animator.Play("Jump up");
+            }
+            _rig.AddForce(new Vector2(0f, jumpForce*2f), ForceMode2D.Impulse);
             timer = 0f;
         }
     }
@@ -51,7 +60,9 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground") && _rig.velocity.y == 0)
         {
+            _animator.Play("Jump land");
             _grounded = true;
+            _falling = false;
         }
     }
     private void OnCollisionStay2D(Collision2D collision)
@@ -73,7 +84,7 @@ public class PlayerMovement : MonoBehaviour
         _asset.Enable();
         _asset.FindAction("Player/Move").started += HandleMove;
         _asset.FindAction("Player/Move").performed += HandleMove;
-        _asset.FindAction("Player/Move").canceled += HandleMove;
+        _asset.FindAction("Player/Move").canceled += HandleMoveStop;
 
         _asset.FindAction("Player/Jump").started += HandleJump;
         _asset.FindAction("Player/Jump").performed += HandleJump;
@@ -84,7 +95,7 @@ public class PlayerMovement : MonoBehaviour
         _asset.FindAction("Player/Dash").canceled += HandleDashCancel;
         
         _asset.FindAction("Player/Vertical").started += HandleUp;
-        _asset.FindAction("Player/Veritcal").performed += HandleUp;
+        _asset.FindAction("Player/Vertical").performed += HandleUp;
         _asset.FindAction("Player/Vertical").canceled += HandleUp;
     }
     private void OnDisable()
@@ -92,7 +103,7 @@ public class PlayerMovement : MonoBehaviour
         _asset.Disable();
         _asset.FindAction("Player/Move").started -= HandleMove;
         _asset.FindAction("Player/Move").performed -= HandleMove;
-        _asset.FindAction("Player/Move").canceled -= HandleMove;
+        _asset.FindAction("Player/Move").canceled -= HandleMoveStop;
 
         _asset.FindAction("Player/Jump").started -= HandleJump;
         _asset.FindAction("Player/Jump").performed -= HandleJump;
@@ -103,11 +114,17 @@ public class PlayerMovement : MonoBehaviour
         _asset.FindAction("Player/Dash").canceled -= HandleDashCancel;
 
         _asset.FindAction("Player/Vertical").started -= HandleUp;
-        _asset.FindAction("Player/Veritcal").performed -= HandleUp;
+        _asset.FindAction("Player/Vertical").performed -= HandleUp;
         _asset.FindAction("Player/Vertical").canceled -= HandleUp;
     }
     public void HandleMove(InputAction.CallbackContext ctx)
     {
+        _animator.Play("Walk");
+        _axisx = ctx.ReadValue<float>();
+    } 
+    public void HandleMoveStop(InputAction.CallbackContext ctx)
+    {
+        _animator.Play("Idle");
         _axisx = ctx.ReadValue<float>();
     } 
     public void HandleUp(InputAction.CallbackContext ctx)
@@ -118,7 +135,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_grounded)
         {
-            _rig.AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
+            if (_falling == false)
+            {
+                _animator.Play("Jump up");
+            }
+            _rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             timer = 0f;
         }
         else if (!ctx.canceled)
@@ -134,17 +155,25 @@ public class PlayerMovement : MonoBehaviour
     }
     public void HandleDash(InputAction.CallbackContext ctx)
     {
-        if (_axisx != 0 || _axisy != 0)
+        if ((_axisx != 0 || _axisy != 0) && !_hasDashed)
         {
-            _rig.AddForce(new Vector2(5f * _axisx, 8f * _axisy), ForceMode2D.Impulse);
+            _animator.Play("Dash");
+            _rig.velocity = Vector2.zero;
+            _rig.AddForce(new Vector2(7f * _axisx, 10f * _axisy), ForceMode2D.Impulse);
+            _hasDashed = true;
         }
     }
     public void HandleDashCancel(InputAction.CallbackContext ctx)
     {
-            _rig.velocity = new Vector2(0f,0f);
+        _rig.velocity = new Vector2(0f,0f);
+        _hasDashed = false;
     }
     public void HandleJumpDown(InputAction.CallbackContext ctx)
     {
-        _rig.AddForce(new Vector2(0f, -7f), ForceMode2D.Impulse);
+        if (!_hasDashed)
+        {
+            _animator.Play("Jump down");
+            _rig.AddForce(new Vector2(0f, -7f), ForceMode2D.Impulse);
+        }
     }
 }
